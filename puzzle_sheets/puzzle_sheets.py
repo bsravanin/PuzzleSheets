6ec998e13caf9b61e476830acef08b39537b8635
Sravan Bhamidipati <bsravanin@gmail.com>
2021-11-16 23:07:05 -0500

    Function to get a better grid for solved puzzles
    This grid is a more readable dict than contains all clues, indices, lengths, answers in one
    object. Instead of having to rely on puz.Puzzle's fill attribute.
    
    This lays the base for creating simple tools around answers, to help with brute-force approaches.

--- puzzle_sheets/puzzle_sheets.py
+++ puzzle_sheets/puzzle_sheets.py
@@ -1,37 +1,82 @@
 #! /usr/bin/env python3
+from collections import OrderedDict
+from collections import namedtuple
+from puz import Puzzle
 
 import argparse
+import os
 import puz
+import sys
+
+
+Solution = namedtuple('Solution', ('num', 'clue', 'len', 'answer'))
 
 
 def _create_parser():
+    """Creates arg parser."""
     parser = argparse.ArgumentParser(description='Interface for Puzzle Sheets.')
     parser.add_argument('-p', '--puz', action='store', required=True, help='Path to PUZ file to be read.')
     return parser.parse_args()
 
 
-def get_clue_str(clue):
-    return f'{clue["num"]}. {clue["clue"]} ({clue["len"]})'
+def _get_across_answer(puzzle: Puzzle, clue: dict) -> str:
+    """Returns the across answer for a given clue in the puzzle. Any blanks will be indicated by dashes."""
+    return puzzle.fill[clue['cell'] : clue['cell'] + clue['len']]
+
+
+def _get_down_answer(puzzle: Puzzle, clue: dict) -> str:
+    """Returns the down answer for a given clue in the puzzle. Any blanks will be indicated by dashes."""
+    return ''.join([puzzle.fill[clue['cell'] + i * puzzle.width] for i in range(clue['len'])])
+
+
+def get_solution_grid(puzzle: Puzzle) -> dict:
+    """Returns a dict that contains the whole grid. The dict has 2 keys: across and down. The value of each is
+    an OrderedDict whose key is the clue number, and value is a Solution namedtuple."""
+    numbering = puzzle.clue_numbering()
+    return {
+        'across': OrderedDict(
+            {
+                clue['num']: Solution(clue['num'], clue['clue'], clue['len'], _get_across_answer(puzzle, clue))
+                for clue in numbering.across
+            }
+        ),
+        'down': OrderedDict(
+            {
+                clue['num']: Solution(clue['num'], clue['clue'], clue['len'], _get_down_answer(puzzle, clue))
+                for clue in numbering.down
+            }
+        ),
+    }
+
+
+def _get_clue_str(solution: Solution) -> str:
+    """Returns a string representation of the clue, for printing."""
+    return f'{solution.num}. {solution.clue} ({solution.len})'
 
 
 def main():
     """Main function that will be run if this is used as a script instead of imported."""
     args = _create_parser()
 
-    puzzle = puz.read(args.puz)
+    puz_path = args.puz
+    if not os.path.isfile(puz_path):
+        print('Cannot read file ', puz_path)
+        sys.exit(-1)
+
+    puzzle = puz.read(puz_path)
+    grid = get_solution_grid(puzzle)
 
     print('ACROSS')
-    for clue in puzzle.clue_numbering().across:
-        print(get_clue_str(clue))
+    for solution in grid['across'].values():
+        print(_get_clue_str(solution))
 
     print('DOWN')
-    for clue in puzzle.clue_numbering().down:
-        print(get_clue_str(clue))
+    for solution in grid['down'].values():
+        print(_get_clue_str(solution))
 
     for row in range(puzzle.height):
         cell = row * puzzle.width
-        # Substitute puzzle.solution for p.fill to print the answers
-        print('\t'.join(puzzle.fill[cell : cell + puzzle.width]).replace('.', chr(9608)))
+        print('\t'.join(puzzle.fill[cell : cell + puzzle.width]).replace('.', chr(9608) * 2))
 
     print(f'Title: {puzzle.title}')
     print(f'Author: {puzzle.author}')
